660b113aa09d26dcb7361b61267bf99d7e94b92f
Pyco <pycobusiness@gmail.com>
2022-11-24 17:44:20 +0000

    changed newbrowser to class.
    Signed-off-by: Pyco <pycobusiness@gmail.com>

--- pylenium/chrome.py
+++ pylenium/chrome.py
@@ -3,13 +3,26 @@ from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 
-def newbrowser(headless=False, detached=False):
-    options = Options()
-    if headless:
-        options.add_argument("--headless")
+# def newbrowser(headless=False, detached=False):
+#     options = Options()
+#     if headless:
+#         options.add_argument("--headless")
 
-    if detached:
-        options.add_experimental_option("detach", True)
+#     if detached:
+#         options.add_experimental_option("detach", True)
     
-    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
-    return driver
+#     driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
+#     return driver
+
+class newbrowser:
+    def __init__(self, headless=False, detached=True):
+        self.options = Options()
+        if headless:
+            self.options.add_argument("--headless")
+
+        if detached:
+            self.options.add_experimental_option("detach", True)
+
+    def getdriver(self):
+        return webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=self.options)
+        
