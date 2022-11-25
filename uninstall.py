d3d0b84dbcf3ff7e9af7febb0e3e1331dada7fc9
Pyco <pycobusiness@gmail.com>
2022-11-25 13:32:54 +0000

    fixed uninstall.py __pycache__

--- uninstall.py
+++ uninstall.py
@@ -11,14 +11,33 @@ def main():
         print(f"{name} is not installed.")
         return
 
+    pycache = True
 
     for file in os.listdir(dest):
         if os.path.isfile(dest + file):
             os.remove(dest + file)
             print("deleted file.")
 
+        try:
+            for file1 in os.listdir(dest + "__pycache__\\"):
+                if os.path.isfile(dest + "__pycache__\\" + file1):
+                    os.remove(dest + "__pycache__\\" + file1)
+                    print("clearing pycache")
+
+        except:
+            print("pycache not found.")
+            pycache = False
+
+    if pycache:
+        try:
+            os.rmdir(dest + "__pycache__")
+            print("removed pycache.")
+        except:
+            pass
+
     os.rmdir(dest)
-    print("deleted directory.")
+    print("removed pylenium.")
+
 
     print("UNINSTALLED SUCCESSFULLY.")
 
